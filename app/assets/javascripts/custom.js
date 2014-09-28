f4b75244feb7dd9ee2ad12bb1e8867467f787e3a
mshmsh <wannabemillioner@gmail.com>
2014-09-28 20:46:44 +0300

    Beautify the code

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,6 @@
-var current_item, section_fadein, section_fadeout;
+var current_tag, section_fadein, section_fadeout;
 
-current_item = 0;
+current_tag = 0;
 
 section_fadeout = 800;
 
@@ -9,12 +9,12 @@ section_fadein = 800;
 $(document).ready(function($) {
   $("a", ".nav").click(function() {
     if (!$(this).hasClass("active")) {
-      current_item = this;
+      current_tag = this;
       $(".section:visible").fadeOut(section_fadeout, function() {
         var new_section;
         $("a", ".nav").removeClass("active");
-        $(current_item).addClass("active");
-        new_section = $($(current_item).attr("href"));
+        $(current_tag).addClass("active");
+        new_section = $($(current_tag).attr("href"));
         new_section.fadeIn(section_fadein);
       });
     }
@@ -26,11 +26,11 @@ $(document).ready(function($) {
   $("#navMove").click(function() {
     $("#nav").toggleClass("nav-toggle");
     if ($("#navMove i").hasClass("fa fa-toggle-on")) {
-      $(" #navMove i").removeClass("fa fa-toggle-on");
-      $(" #navMove i").addClass("fa fa-toggle-off");
+      $("#navMove i").removeClass("fa fa-toggle-on");
+      $("#navMove i").addClass("fa fa-toggle-off");
     } else {
-      $(" #navMove i").removeClass("fa fa-toggle-off")
-      $(" #navMove i").addClass("fa fa-toggle-on");
+      $("#navMove i").removeClass("fa fa-toggle-off")
+      $("#navMove i").addClass("fa fa-toggle-on");
     }
   });
 });
